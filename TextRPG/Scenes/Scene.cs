a59c7f72a5e143c34f7041ee240b889954ae2849
jungtaek kim <kgajungtaek@gmail.com>
2025-04-01 12:57:06 +0900

    [docs] 씬 클래스 주석 추가
    씬 클래스의 주석 내용을 구현하며 다양한 TextRPG 상황을 구현해보자

--- TextRPG/Scenes/Scene.cs
+++ TextRPG/Scenes/Scene.cs
@@ -10,7 +10,14 @@ namespace TextRPG.Scenes
     {
         protected ConsoleKey input;
 
+        /// <summary>
+        /// 씬의 상황 설명을 작성
+        /// </summary>
         public abstract void Render();
+
+        /// <summary>
+        /// 씬의 선택지 목록을 작성
+        /// </summary>
         public abstract void Choice();
 
         public void Input()
@@ -18,8 +25,19 @@ namespace TextRPG.Scenes
             input = Console.ReadKey(true).Key;
         }
 
+        /// <summary>
+        /// 선택한 선택지를 기준으로 결과 출력
+        /// </summary>
         public abstract void Result();
+
+        /// <summary>
+        /// 결과를 본 후 기다리기 기능 구현
+        /// </summary>
         public abstract void Wait();
+
+        /// <summary>
+        /// 다음으로 넘어갈 씬 또는 게임오버를 구현
+        /// </summary>
         public abstract void Next();
     }
 }
